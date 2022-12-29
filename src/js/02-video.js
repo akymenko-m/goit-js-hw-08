956435a61293734e01d8c4eccefab1f8593e646f
Mariia Akymenko <dafnarianna@gmail.com>
2022-12-29 18:50:31 +0200

    make task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+    id: "vimeo-player"
+});
+
+let currentTime;
+
+const onPlay = function (data) {
+    currentTime = JSON.stringify(data.seconds);
+
+    localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+if (savedTime === null) {
+    return;
+}
+
+player.setCurrentTime(savedTime);
